f69b6aa88b1d94f757f96a0b48eb96fd46626111
arydiaihdahusnayainfe <arydiaihdahusnayainfe@gmail.com>
2023-12-12 13:22:35 +0700

    Fix Section SeeMore

--- src/components/see_more/SeeMore.jsx
+++ src/components/see_more/SeeMore.jsx
@@ -3,7 +3,7 @@ import DoubleRight from "../../assets/svg/DoubleRight.svg";
 function SeeMore() {
   return (
     <>
-      <section id="see-more" className="mt-2 text-white rounded-3xl bg-gradient-to-r from-[#8044E2] to-[#E2446A] hover:bg-gradient-to-l from-[#E2446A] to-[#8044E2] transition-all p-[3px]">
+      <section id="see-more" className="mt-2 text-white rounded-3xl bg-gradient-to-r from-[#8044E2] to-[#E2446A] hover:bg-gradient-to-l transition-all p-[3px]">
         <div className="group m-auto bg-section px-10 py-6 rounded-3xl justify-around items-center flex md:w-[99%]">
           <div className="text-[32px] md:text-[45px] cabinet-grotesk-bold leading-10">
             See more
