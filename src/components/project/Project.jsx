0752c6469e47980af011d6325d5a119dd95b812c
arydiaihdahusnayainfe <arydiaihdahusnayainfe@gmail.com>
2023-12-20 13:40:18 +0700

    Config Data Section Project

--- src/components/project/Project.jsx
+++ src/components/project/Project.jsx
@@ -1,123 +1,23 @@
-import ArrowBtn from "../../assets/svg/ArrowButton.svg";
+import CardProject from "../../data/project/CardProject";
+import dataProject from "../../data/project/dataProject";
 
-function Project() {
+const Project = () => {
+  const limitedData = dataProject.slice(0, 3); // Ambil hanya 3 data pertama
+
   return (
     <>
-      <section id="project" className="mt-2 lg:mt-5 text-white">
-        {/* Project section */}
-        <div className="bg-section mt-2 lg:mt-5 rounded-3xl p-2 lg:max-h-[640px]">
-          <div className="py-5 px-3 lg:px-10 lg:flex lg:justify-between">
-            <div>
-              {/* Project title */}
-              <h2 className="text-[32px] md:text-[50px] cabinet-grotesk-bold">
-                ILM
-              </h2>
-              {/* Project description */}
-              <p className="text-[16px] md:text-[25px] cabinet-grotesk-medium text-[#8A8B90]">
-                Educational quiz game
-              </p>
-            </div>
-            <div className="mt-4 lg:w-[50%]">
-              {/* Project details */}
-              <p className="text-[16px] md:text-[25px] cabinet-grotesk-medium">
-                Learn Islamic knowledge from answering quizez and get coin!
-              </p>
-            </div>
-          </div>
-          <div className="bg-slate-400 m-auto h-[95%] rounded-3xl mt-8 relative">
-            <div className="overflow-hidden rounded-3xl">
-              {/* Project image */}
-              <img
-                className="bg-cover bg-center bg-no-repeat w-full h-[305px] object-cover"
-                src="https://source.unsplash.com/random?transportation"
-                alt=""
-              />
-            </div>
-            {/* Arrow button */}
-            <a href="#">
-              <div className="absolute right-5 bottom-5 bg-white p-4 rounded-full hover:bg-slate-500 transition-all">
-                <img className="md:w-[35px]" src={ArrowBtn} alt="" />
-              </div>
-            </a>
-          </div>
-        </div>
-        {/* Project section */}
-        <div className="bg-section mt-2 lg:mt-5 rounded-3xl p-2">
-          <div className="py-5 px-3 lg:px-10 lg:flex lg:justify-between">
-            <div>
-              {/* Project title */}
-              <h2 className="text-[32px] md:text-[50px] cabinet-grotesk-bold">
-                ILM
-              </h2>
-              {/* Project description */}
-              <p className="text-[16px] md:text-[25px] cabinet-grotesk-medium text-[#8A8B90]">
-                Educational quiz game
-              </p>
-            </div>
-            <div className="mt-4 lg:w-[50%]">
-              {/* Project details */}
-              <p className="text-[16px] md:text-[25px] cabinet-grotesk-medium">
-                Learn Islamic knowledge from answering quizez and get coin!
-              </p>
-            </div>
-          </div>
-          <div className="bg-slate-400 m-auto h-[95%] rounded-3xl mt-8 relative">
-            <div className="overflow-hidden rounded-3xl">
-              {/* Project image */}
-              <img
-                className="bg-cover bg-center bg-no-repeat w-full h-[305px] object-cover"
-                src="https://source.unsplash.com/random?transportation"
-                alt=""
-              />
-            </div>
-            {/* Arrow button */}
-            <a href="#">
-              <div className="absolute right-5 bottom-5 bg-white p-4 rounded-full hover:bg-slate-500 transition-all">
-                <img className="md:w-[35px]" src={ArrowBtn} alt="" />
-              </div>
-            </a>
-          </div>
-        </div>
-        {/* Project section */}
-        <div className="bg-section mt-2 lg:mt-5 rounded-3xl p-2">
-          <div className="py-5 px-3 lg:px-10 lg:flex lg:justify-between">
-            <div>
-              {/* Project title */}
-              <h2 className="text-[32px] md:text-[50px] cabinet-grotesk-bold">
-                ILM
-              </h2>
-              {/* Project description */}
-              <p className="text-[16px] md:text-[25px] cabinet-grotesk-medium text-[#8A8B90]">
-                Educational quiz game
-              </p>
-            </div>
-            <div className="mt-4 lg:w-[50%]">
-              {/* Project details */}
-              <p className="text-[16px] md:text-[25px] cabinet-grotesk-medium">
-                Learn Islamic knowledge from answering quizez and get coin!
-              </p>
-            </div>
-          </div>
-          <div className="bg-slate-400 m-auto h-[95%] rounded-3xl mt-8 relative">
-            <div className="overflow-hidden rounded-3xl">
-              {/* Project image */}
-              <img
-                className="bg-cover bg-center bg-no-repeat w-full h-[305px] object-cover"
-                src="https://source.unsplash.com/random?transportation"
-                alt=""
-              />
-            </div>
-            {/* Arrow button */}
-            <a href="#">
-              <div className="absolute right-5 bottom-5 bg-white p-4 rounded-full hover:bg-slate-500 transition-all">
-                <img className="md:w-[35px]" src={ArrowBtn} alt="" />
-              </div>
-            </a>
-          </div>
-        </div>
-      </section>
+      {limitedData.map((data, index) => (
+        <CardProject
+          key={index}
+          title={data.title}
+          subtitle={data.subtitle}
+          description={data.description}
+          image={data.image}
+          link={data.link}
+        />
+      ))}
     </>
   );
-}
+};
 
 export default Project;
---
+++ src/data/project/dataProject.js
@@ -0,0 +1,38 @@
+import ProjectFe1 from "../../assets/png/project-fe-1.png";
+import ProjectFe2 from "../../assets/png/project-fe-2.png";
+import ProjectFe3 from "../../assets/png/project-fe-3.png";
+
+const dataProject = [
+    {
+      title: 'To-Do List',
+      subtitle: 'Project To-Do List',
+      description: 'Fun To-Do List project made with React JS',
+      image: ProjectFe1,
+      link: 'https://github.com/arydiaihdahusnayainfe/tugas-FE-1',
+    },
+    {
+      title: 'Modal',
+      subtitle: 'Project Modal',
+      description: 'Fun Modal project made with React JS',
+      image: ProjectFe2,
+      link: 'https://github.com/arydiaihdahusnayainfe/tugas-FE-2',
+    },
+    {
+      title: 'Slider',
+      subtitle: 'Project Slider',
+      description: 'Fun Slider project made with React JS',
+      image: ProjectFe3,
+      link: 'https://github.com/arydiaihdahusnayainfe/tugas-FE-3',
+    },
+    {
+      title: 'Slider',
+      subtitle: 'Project Slider',
+      description: 'Fun Slider project made with React JS',
+      image: ProjectFe3,
+      link: 'https://github.com/arydiaihdahusnayainfe/tugas-FE-3',
+    },
+    // Anda bisa menambahkan lebih banyak data card sesuai kebutuhan
+  ];
+  
+  export default dataProject;
+  
